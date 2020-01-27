75466aa62154df6ae983fe4818b5f157d9ed4add
uWinAdam <lang111@uwindsor.ca>
2020-01-27 18:30:14 -0500

    improvements

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -24,49 +24,60 @@ router.get("/", auth, async (req, res) => {
 // @route   POST api/auth
 // @desc    Authenticate User and get token
 // @access  Public
-router.post("/", async (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
-
-  const { email, password } = req.body;
-
-  try {
-    // ~~See if user exists~~
-    let user = await User.findOne({ email });
-
-    if (!user) {
-      return res.status(400).json({ errors: [{ msg: "Invalid Credentials" }] });
+router.post(
+  "/",
+  [
+    check("email", "Please include a valid email").isEmail(),
+    check("password", "Password is required").exists()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
 
-    // Matching password
-    const isMatch = await bcrypt.compare(password, user.password);
+    const { email, password } = req.body;
 
-    if (!isMatch) {
-      return res.status(400).json({ errors: [{ msg: "Invalid Credentials" }] });
+    try {
+      // ~~See if user exists~~
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid Credentials" }] });
+      }
+
+      // Matching password
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "Invalid Credentials" }] });
+      }
+
+      // Return jsonwebtoken
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+      // CHANGE BACK TO 3600
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
     }
-
-    // Return jsonwebtoken
-    const payload = {
-      user: {
-        id: user.id
-      }
-    };
-    // CHANGE BACK TO 3600
-    jwt.sign(
-      payload,
-      config.get("jwtSecret"),
-      { expiresIn: 360000 },
-      (err, token) => {
-        if (err) throw err;
-        res.json({ token });
-      }
-    );
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send("Server Error");
   }
-});
+);
 
 module.exports = router;
